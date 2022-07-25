228823bdd5b18bd8076c0cdb60d2546ef5dc0405
brunocriacoes <br.rafael@outlook.com>
2022-07-25 16:55:53 -0300

    ajuste text let's

--- components/Lets/index.js
+++ components/Lets/index.js
@@ -10,7 +10,7 @@ export default function Lets() {
             }}>
                 <div className=" flex items-center justify-items-start  lg:h-screen w-full">
                     <div className="pl-5 lg:pl-20">
-                        <h1 className="block text-[78px] lg:text-[240px] text-Light-Orange font-Beastly font-normal lg:leading-[270px]">lets</h1>
+                        <h1 className="block text-[78px] lg:text-[240px] text-Light-Orange font-Beastly font-normal lg:leading-[270px]">let&apos;s</h1>
                         <div className='text-xs lg:text-lg text-Light-Orange font-TTHoves uppercase font-normal'>
                             <span>
                                 PRONTO PARA USO<br />
